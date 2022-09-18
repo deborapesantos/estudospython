998051038323715440f61b5fccccfcef2b99ea6e
Debora Santos <44837030+deborapesantos@users.noreply.github.com>
2022-09-18 19:15:21 -0300

    update initial

--- initial.py
+++ initial.py
@@ -1,1 +1,24 @@
 
+idade= 26
+altura=1.57
+nome="Debora"
+brasileira=True
+
+print(idade,altura,nome,brasileira)
+
+nomeusuario = input("digite seu nome")
+
+#comentário em python
+'''
+bloco de comentário em python
+'''
+
+#exemplo interpolação
+print(f"seu nome é {nomeusuario}")
+
+
+
+
+
+
+
